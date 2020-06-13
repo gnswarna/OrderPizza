fde9e28d896e64ca1d969d5406950cb077419041
gnswarna <swarna.naga.g@gmail.com>
2020-06-13 09:13:15 +0200

    added main function

--- pizza_app.py
+++ pizza_app.py
@@ -6,14 +6,5 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
-@app.route('/users/', methods = ['GET', 'POST', 'PATCH'])
-def users():
-    if request.method == 'GET':
-        return 'in get', 200
-    elif request.method == 'POST':
-        print(request.data)
-        return 'in post', 200
-    elif request.method == 'PATCH':
-        return 'in patch', 200
-    else:
-        return 'Method Not Allowed' , 405
+if __name__ == '__main__':
+   app.run()
