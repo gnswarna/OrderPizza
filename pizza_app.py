131b26ed3819b0afc80893ce894c4463e9a0fa61
gnswarna <swarna.naga.g@gmail.com>
2020-06-15 12:11:04 +0200

    reading prices from json

--- pizza_app.py
+++ pizza_app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
+import json 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
    return render_template('index.html')
+
+@app.route('/prices/')
+def prices():
+   f = open("prices.json", "r")
+   data = json.load(f)
+   return json.dumps(data)
 
 if __name__ == '__main__':
    app.run('localhost', 80)
--- static/JS/main.js
+++ static/JS/main.js
@@ -1,129 +1,33 @@
-var pizza_1=0; var pizza_2=0; var pizza_3=0; var pizza_5=0; var pizza_6=0; var pizza_7=0; var pizza_8=0; var pizza_9=0; var pizza_10=0; var pizza_11=0;
-
-var pizza_1_d=12; var pizza_1_e=10; var pizza_2_d=11; var pizza_2_e=9;
-var pizza_3_d=13; var pizza_3_e=12; var pizza_5_d=12; var pizza_5_e=11;
-var pizza_6_d=14; var pizza_6_e=12; var pizza_7_d=12; var pizza_7_e=10;
-var pizza_8_d=12; var pizza_8_e=11; var pizza_9_d=12; var pizza_9_e=10;
-var pizza_10_d=14; var pizza_10_e=12; var pizza_11_d=12; var pizza_11_e=11;
-
-var dc_e=5; var dc_d=7;var sub_total_d=0; var total_d=0;var sub_total_e=0; var total_e=0;
-
-
-
-
 $( document ).ready(function() {
+	
+	$.ajax({
+            url: "/prices/",
+            type: "GET",
+            contentType: 'application/json; charset=utf-8',
+            success: function(resultData) {
+		localStorage.setItem('pizza_data', resultData);
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+		console.log(textStatus)
+            },
+        });
 
-	localStorage.setItem('pizza_1_d', '12');
-	localStorage.setItem('pizza_2_d', '11');
-	localStorage.setItem('pizza_3_d', '13');
-	localStorage.setItem('pizza_5_d', '12');
-	localStorage.setItem('pizza_6_d', '14');
-	localStorage.setItem('pizza_7_d', '12');
-	localStorage.setItem('pizza_8_d', '12');
-	localStorage.setItem('pizza_9_d', '12');
-	localStorage.setItem('pizza_10_d', '14');
-	localStorage.setItem('pizza_11_d', '12');
-
-	localStorage.setItem('pizza_1_e', '10');
-	localStorage.setItem('pizza_2_e', '9');
-	localStorage.setItem('pizza_3_e', '12');
-	localStorage.setItem('pizza_5_e', '11');
-	localStorage.setItem('pizza_6_e', '12');
-	localStorage.setItem('pizza_7_e', '10');
-	localStorage.setItem('pizza_8_e', '11');
-	localStorage.setItem('pizza_9_e', '10');
-	localStorage.setItem('pizza_10_e', '12');
-	localStorage.setItem('pizza_11_e', '11');
-
-
-	localStorage.setItem('dc_e', '5');
-	localStorage.setItem('dc_d', '7');
+	localStorage.setItem('cart_count', '0');
 	localStorage.setItem('sub_total_e', '0');
 	localStorage.setItem('sub_total_d', '0');
 	localStorage.setItem('total_d', '0');
 	localStorage.setItem('total_e', '0');
 
-	$('#login').click(onloginclick)
-	$('#orders').click(onordersclick)
-	$('#cart').click(oncartclick)
-	$('#menu').click(onmenuclick)
-
-	$('#login_but').click(onloginbutclick)
-	$('#forgot_password').click(onforgotpwdclick)
-	$('#new_user').click(onnewuserclick)
-	$('#confirm_order').click(onconfirmorderclick)
-	$('#menutocart').click(oncartclick)
-	$('#carttomenu').click(onmenuclick)
-	$('#carttoorder').click(oncarttoorderclick)
-	$('#login_email').change(onloginemailchange)
-	$('#update_pwd').click(onupdatepwdclick)
-	$('#register').click(onregisterclick)
-
-	$('#1_plus').click(onplusclick)
-	$('#1_minus').click(onminusclick)
-	$('#1_text').change(ontextclick)
-	$('#2_plus').click(onplusclick)
-	$('#2_minus').click(onminusclick)
-	$('#2_text').change(ontextclick)
-	$('#3_plus').click(onplusclick)
-	$('#3_minus').click(onminusclick)
-	$('#3_text').change(ontextclick)
-	$('#5_plus').click(onplusclick)
-	$('#5_minus').click(onminusclick)
-	$('#5_text').change(ontextclick)
-	$('#6_plus').click(onplusclick)
-	$('#6_minus').click(onminusclick)
-	$('#6_text').change(ontextclick)
-	$('#7_plus').click(onplusclick)
-	$('#7_minus').click(onminusclick)
-	$('#7_text').change(ontextclick)
-	$('#8_plus').click(onplusclick)
-	$('#8_minus').click(onminusclick)
-	$('#8_text').change(ontextclick)
-	$('#9_plus').click(onplusclick)
-	$('#9_minus').click(onminusclick)
-	$('#9_text').change(ontextclick)
-	$('#10_plus').click(onplusclick)
-	$('#10_minus').click(onminusclick)
-	$('#10_text').change(ontextclick)
-	$('#11_plus').click(onplusclick)
-	$('#11_minus').click(onminusclick)
-	$('#11_text').change(ontextclick)
-	
-	$('#1_cartplus').click(onplusclick)
-	$('#1_cartminus').click(onminusclick)
-	$('#1_carttext').change(ontextclick)
-	$('#2_cartplus').click(onplusclick)
-	$('#2_cartminus').click(onminusclick)
-	$('#2_carttext').change(ontextclick)
-	$('#3_cartplus').click(onplusclick)
-	$('#3_cartminus').click(onminusclick)
-	$('#3_carttext').change(ontextclick)
-	$('#5_cartplus').click(onplusclick)
-	$('#5_cartminus').click(onminusclick)
-	$('#5_carttext').change(ontextclick)
-	$('#6_cartplus').click(onplusclick)
-	$('#6_cartminus').click(onminusclick)
-	$('#6_carttext').change(ontextclick)
-	$('#7_cartplus').click(onplusclick)
-	$('#7_cartminus').click(onminusclick)
-	$('#7_carttext').change(ontextclick)
-	$('#8_cartplus').click(onplusclick)
-	$('#8_cartminus').click(onminusclick)
-	$('#8_carttext').change(ontextclick)
-	$('#9_cartplus').click(onplusclick)
-	$('#9_cartminus').click(onminusclick)
-	$('#9_carttext').change(ontextclick)
-	$('#10_cartplus').click(onplusclick)
-	$('#10_cartminus').click(onminusclick)
-	$('#10_carttext').change(ontextclick)
-	$('#11_cartplus').click(onplusclick)
-	$('#11_cartminus').click(onminusclick)
-	$('#11_carttext').change(ontextclick)
-
-
-
-
+	localStorage.setItem('pizza_1', '0');
+	localStorage.setItem('pizza_2', '0');
+	localStorage.setItem('pizza_3', '0');
+	localStorage.setItem('pizza_5', '0');
+	localStorage.setItem('pizza_6', '0');
+	localStorage.setItem('pizza_7', '0');
+	localStorage.setItem('pizza_8', '0');
+	localStorage.setItem('pizza_9', '0');
+	localStorage.setItem('pizza_10', '0');
+	localStorage.setItem('pizza_11', '0');
 });
 
 function removeactive(){
@@ -131,7 +35,6 @@ function removeactive(){
 	$("#orders").removeClass("active");
 	$("#cart").removeClass("active");
 	$("#menu").removeClass("active");
-
 	$("#login_div").addClass("hide");
 	$("#forgotpwd_div").addClass("hide");
 	$("#newuser_div").addClass("hide");
@@ -146,16 +49,19 @@ function removeactive(){
 function carthandling(){
 	sub_total_d = 0;
 	sub_total_e = 0;
+	cart_count = 0;
+	pizza_data = $.parseJSON(localStorage.getItem('pizza_data'));
 	for (var i = 0; i < 12; i++) {
 		if(i != 4){
   			if(localStorage.getItem('pizza_'+i) > 0 ){
 				
 				$("#cart_"+i).removeClass("hide");
-				dollr = Number(localStorage.getItem('pizza_'+i)) * Number(localStorage.getItem('pizza_'+i+'_d'));
+				dollr = Number(localStorage.getItem('pizza_'+i)) * Number(pizza_data['pizza_'+i+'_d']);
 				sub_total_d = Number(sub_total_d) + dollr; 
-				euro = Number(localStorage.getItem('pizza_'+i)) * Number(localStorage.getItem('pizza_'+i+'_e'));
+				euro = Number(localStorage.getItem('pizza_'+i)) * Number(pizza_data['pizza_'+i+'_e']);
 				sub_total_e = Number(sub_total_e) + euro; 
 				$("#"+i+"_cartcost").text('€'+ euro +' / $' + dollr);
+				cart_count = cart_count + Number(localStorage.getItem('pizza_'+i))
 			}
 			else{
 				$("#cart_"+i).addClass("hide");
@@ -166,11 +72,16 @@ function carthandling(){
 	localStorage.setItem('sub_total_d',sub_total_d);
 	localStorage.setItem('sub_total_e',sub_total_e);
 	$("#cart_total").text('Total : € '+  sub_total_e +' / $ '+ sub_total_d);
-
+	if(cart_count > 0){
+		$("#cart_number").text('Cart ('+  cart_count +')');
+	}
+	else{	
+		$("#cart_number").text('Cart');
+	}
 }
 
-function onplusclick(){
-	id=this.id.split('_')[0];
+function onplusclick(type){
+	id=$(type).attr('id').split('_')[0];
 	text_id = id+'_text';
 	value = Number($('#'+text_id).val());
 	$('#'+text_id).val(value+1)
@@ -181,8 +92,8 @@ function onplusclick(){
 	
 }
 
-function onminusclick(){
-	id=this.id.split('_')[0];
+function onminusclick(type){
+	id=$(type).attr('id').split('_')[0];
 	text_id = id+'_text';
 	value = Number($('#'+text_id).val());
 	if(value > 0){
@@ -194,8 +105,8 @@ function onminusclick(){
 	carthandling();
 }
 
-function ontextclick(){
-	id=this.id.split('_')[0];
+function ontextclick(type){
+	id=$(type).attr('id').split('_')[0];
 	value = Number($('#'+text_id).val());
 	localStorage.setItem('pizza_'+id,value);
 	text_id = id+'_text';
@@ -207,7 +118,8 @@ function ontextclick(){
 }
 
 function oncarttoorderclick(){	
-	var summary = ''	
+	var summary = ''
+	pizza_data = $.parseJSON(localStorage.getItem('pizza_data'));	
  	removeactive();
 	$("#order_form_div").removeClass("hide");
 	
@@ -221,11 +133,11 @@ function oncarttoorderclick(){
 	}
 	$('#order_description').val(summary);
 
-	$('#order_dc').text('€ ' + localStorage.getItem('dc_e') + '/ $ ' + localStorage.getItem('dc_d'));
+	$('#order_dc').text('€ ' + pizza_data['dc_e'] + '/ $ ' + pizza_data['dc_d']);
 	$('#order_sub_total').text('€ ' + localStorage.getItem('sub_total_e') + '/ $ ' + localStorage.getItem('sub_total_d'));
 
-	localStorage.setItem('total_d', Number(localStorage.getItem('dc_d'))+Number(localStorage.getItem('sub_total_d')));
-	localStorage.setItem('total_e', Number(localStorage.getItem('dc_e'))+Number(localStorage.getItem('sub_total_e')));
+	localStorage.setItem('total_d', Number(pizza_data['dc_d'])+Number(localStorage.getItem('sub_total_d')));
+	localStorage.setItem('total_e', Number(pizza_data['dc_e'])+Number(localStorage.getItem('sub_total_e')));
 
 	$('#order_total').text('€ ' + localStorage.getItem('total_e') + '/ $ ' + localStorage.getItem('total_d'));
 }
@@ -327,16 +239,6 @@ function onloginemailchange(){
 
 }
 
-function onloginemailchange(){
-	email = $('#login_email').val()
-	if(validateEmail(email)){
-		console.log(email)
-	}
-	else{
-		alert('Please enter a valid Email-id')
-	}
-
-}
 
 function onupdatepwdclick(){
 	email = $('#forgotpwd_email').val();
